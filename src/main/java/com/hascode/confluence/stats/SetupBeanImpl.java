eec4710fdf54b5b1aba7c2c0eb9817b5b681ad65
Micha Kops <micha.kops@gmail.com>
2016-05-31 15:21:23 +0200

    Proof of concept for JMX Mbean export in Confluence plugin implemented.

--- src/main/java/com/hascode/confluence/stats/SetupBeanImpl.java
+++ src/main/java/com/hascode/confluence/stats/SetupBeanImpl.java
@@ -1,82 +1,45 @@
 package com.hascode.confluence.stats;
 
 import java.lang.management.ManagementFactory;
-import java.util.List;
 
-import javax.management.InstanceAlreadyExistsException;
-import javax.management.MBeanRegistrationException;
 import javax.management.MBeanServer;
 import javax.management.MalformedObjectNameException;
-import javax.management.NotCompliantMBeanException;
 import javax.management.ObjectName;
 
-import com.atlassian.confluence.event.events.plugin.PluginInstallEvent;
-import com.atlassian.event.api.EventListener;
-import com.atlassian.event.api.EventPublisher;
-import com.atlassian.plugin.PluginAccessor;
-import com.hascode.confluence.stats.plugin.StatsPlugin;
-import com.hascode.confluence.stats.plugin.StatsPluginModuleDescriptor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.atlassian.confluence.user.UserAccessor;
+import com.hascode.confluence.stats.jmx.BasicConfluenceStats;
+import com.hascode.confluence.stats.jmx.BasicConfluenceStatsMBean;
 
 public class SetupBeanImpl implements SetupBean {
-	private final EventPublisher eventPublisher;
-	private final PluginAccessor pluginAccessor;
+	private static final Logger LOG = LoggerFactory.getLogger(SetupBeanImpl.class);
 
-	public SetupBeanImpl(EventPublisher eventPublisher, PluginAccessor pluginAccessor) {
-		this.eventPublisher = eventPublisher;
-		this.pluginAccessor = pluginAccessor;
-		eventPublisher.register(this);
-	}
+	private final UserAccessor userAccessor;
+	private MBeanServer mbs;
+	private ObjectName objectName;
 
-	@EventListener
-	public void on(PluginInstallEvent evt) {
-		System.err.println("XXXXXXXXXXXXXXXXXXX Plugin event caught: " + evt.getClass());
-		try {
-			registerMBeans();
-		} catch (MalformedObjectNameException | InstanceAlreadyExistsException | MBeanRegistrationException
-				| NotCompliantMBeanException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+	public SetupBeanImpl(UserAccessor userAccessor) throws MalformedObjectNameException {
+		this.userAccessor = userAccessor;
+		mbs = ManagementFactory.getPlatformMBeanServer();
+		objectName = new ObjectName("com.hascode.confluence.stats.jmx:type=BasicConfluenceStats");
 	}
 
 	@Override
 	public void afterPropertiesSet() throws Exception {
-		System.err.println("XXXXXXXXXXXXXXXXXXXXXXXXXX SETUP");
-	}
+		LOG.info("setting up JMX statistics MBeans..");
+		BasicConfluenceStatsMBean mBean = new BasicConfluenceStats(userAccessor);
 
-	private void registerMBeans() throws MalformedObjectNameException, InstanceAlreadyExistsException,
-			MBeanRegistrationException, NotCompliantMBeanException {
-		// Get the MBean server
-		MBeanServer mbs = ManagementFactory.getPlatformMBeanServer();
-		List<StatsPluginModuleDescriptor> descriptors = pluginAccessor
-				.getEnabledModuleDescriptorsByClass(StatsPluginModuleDescriptor.class);
-		System.err.println("XXXXXXXXXXXXXXXXXXXXXXXXX " + descriptors.size() + " descriptors found");
-		for (StatsPluginModuleDescriptor descriptor : descriptors) {
-			StatsPlugin module = descriptor.getModule();
-			ObjectName name = createObjectName(module);
-			mbs.registerMBean(module, name);
-		}
-	}
-
-	private ObjectName createObjectName(StatsPlugin module) throws MalformedObjectNameException {
-		ObjectName name = new ObjectName(
-				String.format("%s:type=%s", module.getClass().getPackage().getName(), module.title()));
-		return name;
+		mbs.registerMBean(mBean, objectName);
 	}
 
 	@Override
 	public void destroy() throws Exception {
-		System.err.println("XXXXXXXXXXXXXXXXXXXXXXXXXX SHUTDOWN");
-		// MBeanServer mbs = ManagementFactory.getPlatformMBeanServer();
-		// List<StatsPluginModuleDescriptor> descriptors = pluginAccessor
-		// .getEnabledModuleDescriptorsByClass(StatsPluginModuleDescriptor.class);
-		// System.err.println("XXXXXXXXXXXXXXXXXXXXXXXXX " + descriptors.size()
-		// + " descriptors found");
-		// for (StatsPluginModuleDescriptor descriptor : descriptors) {
-		// StatsPlugin module = descriptor.getModule();
-		// ObjectName name = createObjectName(module);
-		// mbs.unregisterMBean(name);
-		// }
+		LOG.info("shutting down JMX statistics MBeans..");
+		if (mbs != null) {
+			mbs.unregisterMBean(objectName);
+		}
 	}
 
 }
--- src/main/java/com/hascode/confluence/stats/plugin/StatsPlugin.java
+++
@@ -1,9 +0,0 @@
-package com.hascode.confluence.stats.plugin;
-
-public interface StatsPlugin {
-	String title();
-
-	String key();
-
-	Object value();
-}
